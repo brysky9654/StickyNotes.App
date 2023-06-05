0e5103c7fcdfbbf684e7f281bc45d2dd9e7fd50f
brysky9654 <brysky456@outlook.com>
2023-06-06 00:08:24 +0900

    Update : drag, add button resolve : app knock down when I click '+' -> can't add this window's item to other window.
    Issue: when click TextBlock , create new window. I would make this show  present window

--- StickyNotes.App/StickyNotesMainView.xaml.cs
+++ StickyNotes.App/StickyNotesMainView.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace StickyNotes.App
 {
@@ -11,57 +14,104 @@ namespace StickyNotes.App
   {
 
     private StickyNotesView stickyNotesView;
+
+    public struct Linq
+    {
+      public Window win;
+      public TextBlock textBlock;
+    }
+    List<Linq> linq = new();
     public StickyNotesMainView()
     {
       InitializeComponent();
-    }
 
-    protected  void Border_PreviewMouseDownLeft(object sender, MouseButtonEventArgs e)
+  }
+
+
+    protected void Header_PreviewMouseDownLeft(object sender, MouseButtonEventArgs e) 
     {
-    //https://stackoverflow.com/questions/16608523/c-sharp-wpf-move-the-window
-      DragMove();
+      // Grid  에서 마우스 down이벤트 안된 이유 https://stackoverflow.com/questions/12669756/mousedown-doesnt-work-in-grid-only-on-buttons-which-in-grids
+      if (e.ButtonState == MouseButtonState.Pressed)
+      {
+        DragMove();
+      }
+
+
+    }
+    //TODO: TextBlock 클릭시 해당 윈도우창 나타나야함. 단, 1나만 나타나야함. 기존에 이미 나타나져있으면 pass
+    // 윈도우창들을 구분할 수 있는 인덱스 존재여부, 존재한다면 그 윈도우창이 특정 textBlock과 링킹 되도록 해야함. 윈도우창(텍스트박스)에서 삭제 버튼 누르면 textBlock이 삭제될 수 있게.
+    // textBlock에서 삭제 버튼기능이 있어 삭제 누르면 링킹된 윈도우창도 없앨 수 있게.
+    protected  void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      //https://stackoverflow.com/questions/16608523/c-sharp-wpf-move-the-window
+
+
+      //text 이벤트 추가.
+      foreach (Linq x in linq) 
+      {
+        x.textBlock.MouseLeftButtonDown += selectTextBlock_Click;
+      }
+      
     }
     private void closeButton_Click(object sender, RoutedEventArgs e) => Close();
 
     //TODO TextBlock
-    //text block 을 만든 이유 : 텍스트박스와 구분해서 텍스트박스 글자 수가 0이면 텍스트 block에 Search...  을 글자수가 0이아니면 textBlock 글자는 "" 
-    //text box안에 Textblock 또는 Label로 Search... 글자 만들기.
-    // mouse down event : 커서를 텍스트박스에 클릭하면 Label과 Textblock 글자색 얕아지게 만들기. 
-    // keyboard down event : Search... 없애고 입력된 값들을 textbox에 채워지게만들기.
+    /// <summary>
+    /// 워터마크 코드 https://code.4noobz.net/wpf-add-a-watermark-to-a-native-wpf-textbox/
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
 
-    // --> 해결 워터마크 코드 https://code.4noobz.net/wpf-add-a-watermark-to-a-native-wpf-textbox/
+    //TODO New window , TextBox  
+    // 내 코드 분석하기.
+    //새로운 윈도우창에 텍스트 box가 생겨지고 동시에 기존 윈도우창에 textblock이 생겨진다.
+    //그리고 새 윈도우창에 텍스트box 에 textChaned이벤트를 줘서 원래 윈도우창의 textblock에 문자열들이 출력되야한다.
+    // 또한 원래 윈도우창에서 textblock을 클릭하면, lingking된 textbox가 나와서 쓰기모드가 가능해야한다.
+    // + New Note 클릭 시 텍스트 박스가 다른 윈도우창에서 생겨지고 Main 에선 3행에 텍스트 Block이 만들어짐에 따라 글자 입력 시 함께 진행됨. - 해결. 근데 의아함.
+    // 코드분석해보기, textblock을 stack panel에 추가하지 않았고 txtbox와 txtblock을 연결시키지도 않았는데 되네?
 
-    //TODO New window , TextBox
-    // + New Note 클릭 시 텍스트 박스가 다른 윈도우창에서 생겨지고 Main 에선 3행에 텍스트 Block이 만들어짐에 따라 글자 입력 시 함께 진행됨.
+    private void selectTextBlock_Click(object sender, MouseButtonEventArgs e)
+    {
+      var sender_type = sender.GetType();
+      TextBlock sender_textBlock_type = (TextBlock)sender;
 
+      //sender 타입이 textBlock 타입이라면
+      if (sender_type == sender_textBlock_type.GetType())
+      {   
+        //linq 원소들 중에 현재 클릭한 textBlock을 찾아서 Visible 주기.
+        foreach( Linq x in linq)
+         {
+          if(sender_textBlock_type ==x.textBlock)
+          {
+            x.win.Visibility = Visibility.Collapsed;
 
-    //New Window 구성
+            x.win.Visibility = Visibility.Visible;
+
+          }
+
+        }
+      }
+   
+    }
 
     private void addTextBox_Click(object sender, RoutedEventArgs e)
     {
       if(e.Source != null)
       {
 
-        //새로운 윈도우창에 텍스트 box가 생겨지고 동시에 기존 윈도우창에 textblock이 생겨진다.
-        //그리고 새 윈도우창에 텍스트box 에 textChaned이벤트를 줘서 원래 윈도우창의 textblock에 문자열들이 출력되야한다.
-        // 또한 원래 윈도우창에서 textblock을 클릭하면, lingking된 textbox가 나와서 쓰기모드가 가능해야한다.
         TextBlock textBlock = new TextBlock();
+        textBlock.HorizontalAlignment = HorizontalAlignment.Stretch;
+        textBlock.TextAlignment = TextAlignment.Left;
+        textBlock.Background = new SolidColorBrush(Colors.DimGray);
 
+        textBlock.Height = 50;
+        textBlock.Margin = new Thickness(10, 10, 10, 0);
         
         stackPanel_Notes.Children.Add(textBlock);
-        stickyNotesView = new StickyNotesView(textBlock);
 
-
-        //if (textBox.Text.Length > 0)
-        //{
-        //  string fileText = File.ReadAllText(textBox.Text);
-        //}
-
+        stickyNotesView = new StickyNotesView(textBlock, linq);
       }
-
-
     }
-
 
   }
 }
--- StickyNotes.App/StickyNotesView.xaml.cs
+++ StickyNotes.App/StickyNotesView.xaml.cs
@@ -1,49 +1,76 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
+using static StickyNotes.App.StickyNotesMainView;
 
 namespace StickyNotes.App
 {
+
   /// <summary>
   /// Interaction logic for StickyNotesView.xaml
   /// </summary>
-  
-  public partial class StickyNotesView : UserControl
+
+
+
+
+  public partial class StickyNotesView : Window
   {
     private TextBlock _textBlock;
-    public StickyNotesView(TextBlock textBlock)
+    private List<Linq> _Linq;
+
+    public StickyNotesView(TextBlock textBlock, List<Linq> linq)
     {
       InitializeComponent();
       _textBlock = textBlock;
+      _Linq = linq;  // Main 뷰에있는 linq를 _Linq가 참조. _Linq 원소삭제시 linq도 삭제
       newWindow();
 
     }
 
+    //TODO: 윈도우창과 텍스트 블록 링킹되서 윈도우창도 구분되야함. 윈도우창 내 삭제버튼 기능 통해 윈도우창과 텍스트블록 제거하기.
+
+    //TextBlock에 이름 설정. window도 이름설정가능?  가능한가봄./
     private void newWindow() 
     {
+
+      //TODO: 정리하기 win 사용해도 되나?
+      // this 윈도우로 해결할 수 있을 것 같으면 해결.
+      //동일 파일에 윈도우가 2개 있을지라도 다른 윈도우에 만들어진 아이템 이름을 다른 윈도에 사용할 수 없다.
+
+      //TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      //win 과 관련된거 모두 this로 옮기기. 
       Window win = new Window();
-      win.Width = 300;
-      win.Height = 300;
+
+      _Linq.Add(new Linq { win = win, textBlock = _textBlock });
+ 
+      //win.Width = 300;
+      //win.Height = 300;
+
+
+      //linq.Add(new Linq { win = win, textBlock = _textBlock });
 
       SubTextBox.Foreground = new SolidColorBrush(Colors.White);
       SubTextBox.Background = new SolidColorBrush(Colors.Black);
       SubTextBox.TextWrapping = TextWrapping.Wrap;
       SubTextBox.AcceptsReturn = true;  // 개행문자.
+      this.Width = 300;
+      this.Height = 300;
 
-      win.Content = SubTextBox;
-      win.Show();
 
+      win.WindowStyle = WindowStyle.ThreeDBorderWindow;
+
+      this.Show();
     }
     private void TextBox_Changed(object sender, TextChangedEventArgs e)
     {
       if(e.Source != null)
       {
         StickyNotesMainView mainView  = new StickyNotesMainView();
-        //TextBlock textBlock = new TextBlock();
         _textBlock.Text = "";
         _textBlock.Text = SubTextBox.Text;
-        _textBlock.Width = 50;
-        _textBlock.Height = 50;
         _textBlock.Foreground = new SolidColorBrush(Colors.White);
 
         //system.invalidoperationexception: 'specified element is already the logical child of another element. disconnect it first.'
