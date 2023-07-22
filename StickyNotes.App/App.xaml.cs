5d488ac81085fae877caf26d8c2c8ceb81b469ae
brysky9654 <brysky456@outlook.com>
2023-07-22 21:20:23 +0900

    add single instance

--- StickyNotes.App/App.xaml.cs
+++ StickyNotes.App/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+using System.Threading;
+using System.Windows;
 
 namespace StickyNotes.App
 {
@@ -7,5 +8,21 @@ namespace StickyNotes.App
   /// </summary>
   public partial class App : Application
   {
+    /* single instance 프로그램 두개 안띄우는법  */
+    Mutex mutex;
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+
+      string mutexName = "my.StickyNote";
+      bool bCreateNew;
+
+      mutex = new Mutex(true, mutexName, out bCreateNew);
+
+      if (!bCreateNew) // 기존에 있으면 false가 나오기에 !로 true 만들어서 실행되려는 거 shutdown
+      {
+        Shutdown();
+      }
+    }
   }
 }
